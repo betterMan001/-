bdcec3f48e6f9be49fae73d8ea694b40200592f4
betterMan001 <851428195@qq.com>
2018-12-03 17:49:34 +0800

    Initial commit

---
+++ app/src/main/java/com/ly/a316/ly_meetingroommanagement/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ly.a316.ly_meetingroommanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
