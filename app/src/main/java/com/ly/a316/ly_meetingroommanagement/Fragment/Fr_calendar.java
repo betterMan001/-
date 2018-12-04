286c406b4f10c42f7085dd1697e851ce295490d7
1169927533 <1169927533>
2018-12-04 21:07:27 +0800

    增加了日历 2018/12/4 21：07

--- app/src/main/java/com/ly/a316/ly_meetingroommanagement/Fragment/Fr_calendar.java
+++ app/src/main/java/com/ly/a316/ly_meetingroommanagement/Fragment/Fr_calendar.java
@@ -1,39 +1,133 @@
 package com.ly.a316.ly_meetingroommanagement.Fragment;
 
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+
+import android.support.v7.widget.RecyclerView;
+
 import android.view.View;
-import android.view.ViewGroup;
+
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.CalendarLayout;
+import com.haibin.calendarview.CalendarView;
+import com.ly.a316.ly_meetingroommanagement.Class.jilei;
 import com.ly.a316.ly_meetingroommanagement.R;
-import com.ly.a316.ly_meetingroommanagement.Utils.PopupMenuUtil;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ *  描述：日历
+ *  作者： 余智强
+ *  创建时间：12/4 21：06 第一次提交
+*/
+public class Fr_calendar extends jilei implements CalendarView.OnCalendarSelectListener, CalendarView.OnYearChangeListener, View.OnClickListener {
+    View view;
+    TextView tvMonthDay,tvYear,tvLunar,tvCurrentDay;
+    ImageView ibCalendar;
+    FrameLayout flCurrent;
+    RelativeLayout rlTool;
+    CalendarView ibCalendarview;
+    CalendarLayout calendarLayout;
+    RecyclerView calRecycleview;
 
+    private int mYear;
 
-public class Fr_calendar extends Fragment {
-    View view;//整个视图
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_fr_calendar;
+    }
+    @Override
+    protected void initView() {
+        initview();
+    }
+    @Override
+    protected void initData() {
+    }
+    void initview() {
+        setStatusBarDarkMode();
+        view = jilei.view;
+        tvMonthDay = view.findViewById(R.id.tv_month_dayyy);
+        tvYear = view.findViewById(R.id.tv_year);
+        tvLunar = view.findViewById(R.id.tv_lunar);
+        ibCalendar = view.findViewById(R.id.ib_calendar);
+        tvCurrentDay = view.findViewById(R.id.tv_current_day);
+        flCurrent = view.findViewById(R.id.fl_current);
+        rlTool = view.findViewById(R.id.rl_tool);
+        ibCalendarview = view.findViewById(R.id.ib_calendarview);
+        calendarLayout = view.findViewById(R.id.calendarLayout);
+        calRecycleview = view.findViewById(R.id.cal_recycleview);
+        tvMonthDay.setOnClickListener(this);
+        tvCurrentDay.setOnClickListener(this);
+        ibCalendarview.setOnCalendarSelectListener(this);
+        ibCalendarview.setOnYearChangeListener(this);
+        tvYear.setText(String.valueOf(ibCalendarview.getCurYear()));
+        mYear = ibCalendarview.getCurYear();
+        tvMonthDay.setText((String.valueOf(ibCalendarview.getCurMonth()) + "月" + String.valueOf(ibCalendarview.getCurDay()) + "日"));
+        tvLunar.setText("今天");
+        tvCurrentDay.setText(String.valueOf(ibCalendarview.getCurDay()));
 
-    public Fr_calendar() {
+        int year = ibCalendarview.getCurYear();
+        int month = ibCalendarview.getCurMonth();
+
+        Map<String, Calendar> map = new HashMap<>();
+        map.put(getSchemeCalendar(year, month, 3, 0xFF40db25, "假").toString(),
+                getSchemeCalendar(year, month, 3, 0xFF40db25, "假"));
+        ibCalendarview.setSchemeDate(map);
+    }
+
+    private Calendar getSchemeCalendar(int year, int month, int day, int color, String text) {
+        Calendar calendar = new Calendar();
+        calendar.setYear(year);
+        calendar.setMonth(month);
+        calendar.setDay(day);
+        calendar.setSchemeColor(color);//如果单独标记颜色、则会使用这个颜色
+        calendar.setScheme(text);
+        calendar.addScheme(new Calendar.Scheme());
+        calendar.addScheme(0xFF008800, "假");
+        calendar.addScheme(0xFF008800, "节");
+        return calendar;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_fr_calendar, container, false);
-        return view;
+    public void onCalendarOutOfRange(Calendar calendar) {
+
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
+    public void onCalendarSelect(Calendar calendar, boolean isClick) {
+        tvLunar.setVisibility(View.VISIBLE);
+        tvYear.setVisibility(View.VISIBLE);
+        tvMonthDay.setText(calendar.getMonth() + "月" + calendar.getDay() + "日");
+        tvYear.setText(String.valueOf(calendar.getYear()));
+        tvLunar.setText(calendar.getLunar());
+        mYear = calendar.getYear();
+    }
 
+    @Override
+    public void onYearChange(int year) {
+        tvMonthDay.setText(String.valueOf(year));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_month_dayyy:
+                if (!calendarLayout.isExpand()) {
+                    ibCalendarview.showYearSelectLayout(mYear);////快速弹出年份选择月份
+                    return;
+                }
+                ibCalendarview.showYearSelectLayout(mYear);
+                tvLunar.setVisibility(View.GONE);
+                tvYear.setVisibility(View.GONE);
+                tvMonthDay.setText(String.valueOf(mYear));
+                break;
+            case R.id.tv_current_day:
+                ibCalendarview.scrollToCurrent();////滚动到当前日期
+                break;
+        }
     }
 }
--- app/src/main/java/com/ly/a316/ly_meetingroommanagement/MainActivity.java
+++ app/src/main/java/com/ly/a316/ly_meetingroommanagement/MainActivity.java
@@ -1,51 +1,45 @@
 package com.ly.a316.ly_meetingroommanagement;
 
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.FrameLayout;
 
 import com.ly.a316.ly_meetingroommanagement.Class.TabEntity;
 import com.ly.a316.ly_meetingroommanagement.Customview.BottomBarLayout;
 import com.ly.a316.ly_meetingroommanagement.Fragment.Fr_calendar;
 import com.ly.a316.ly_meetingroommanagement.Fragment.Fr_mine;
-import com.ly.a316.ly_meetingroommanagement.Fragment.Fr_work;
 import com.ly.a316.ly_meetingroommanagement.Utils.PopupMenuUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindInt;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
+
 /**
- *  描述：主活动
- *  作者：余智强
- *  创建时间：2018 12/4 13：27
-*/
+ * 描述：主活动
+ * 作者：余智强
+ * 创建时间：2018 12/4 13：27
+ */
 public class MainActivity extends BaseActivity {
     @BindView(R.id.bottom_nav)
-     BottomBarLayout bottomBarLayout;
+    BottomBarLayout bottomBarLayout;
     Fragment fr_calendar, fr_mine;
     private FragmentManager fManager;
 
     private List<TabEntity> tabEntityList;
-    private String[] tabText = {"日程","工作","我的"};
+    private String[] tabText = {"日程", "工作", "我的"};
 
-    private int[] normalIcon = {R.drawable.calendar,R.drawable.bg,R.drawable.me};
-    private int[] selectIcon = {R.drawable.calender2,R.drawable.bg,R.drawable.me2};
+    private int[] normalIcon = {R.drawable.calendar, R.drawable.bg, R.drawable.me};
+    private int[] selectIcon = {R.drawable.calender2, R.drawable.bg, R.drawable.me2};
 
     private int normalTextColor = Color.parseColor("#999999");
     private int selectTextColor = Color.parseColor("#fa6e51");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,14 +48,15 @@ public class MainActivity extends BaseActivity {
         fManager = getSupportFragmentManager();
         tabEntityList = new ArrayList<>();
         initview();
+
         bottomBarLayout.setNormalTextColor(normalTextColor);
         bottomBarLayout.setSelectTextColor(selectTextColor);
         bottomBarLayout.setTabList(tabEntityList);
 
 
-        bottomBarLayout.setOnItemClickListener(new BottomBarLayout.OnItemClickListener(){
+        bottomBarLayout.setOnItemClickListener(new BottomBarLayout.OnItemClickListener() {
             @Override
-            public void onItemCLick(int position,View v) {
+            public void onItemCLick(int position, View v) {
                 FragmentTransaction fTransaction = fManager.beginTransaction();
                 hideAllfragment(fTransaction);
                 switch (position) {
@@ -69,8 +64,8 @@ public class MainActivity extends BaseActivity {
                         /**
                          *   动态改角标
                          *   TextView number = (TextView) v.findViewById(R.id.tv_count);
-                             number.setVisibility(View.GONE);
-                             number.setText("12");
+                         number.setVisibility(View.GONE);
+                         number.setText("12");
                          */
                         if (fr_calendar == null) {
                             fr_calendar = new Fr_calendar();
@@ -97,8 +92,9 @@ public class MainActivity extends BaseActivity {
             }
         });
     }
-    void initview(){
-        for (int i=0;i<tabText.length;i++){
+
+    void initview() {
+        for (int i = 0; i < tabText.length; i++) {
             TabEntity item = new TabEntity();
             item.setText(tabText[i]);
             item.setNormalIconId(normalIcon[i]);
