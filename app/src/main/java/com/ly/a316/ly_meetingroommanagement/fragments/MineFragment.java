a0a7cd6066a24c5df56bfdb64df06512c75e8d32
betterMan001 <851428195@qq.com>
2018-12-07 21:09:37 +0800

    新增我的界面第一版

--- app/src/main/java/com/ly/a316/ly_meetingroommanagement/fragments/MineFragment.java
+++ app/src/main/java/com/ly/a316/ly_meetingroommanagement/fragments/MineFragment.java
@@ -4,16 +4,27 @@ package com.ly.a316.ly_meetingroommanagement.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.ly.a316.ly_meetingroommanagement.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MineFragment extends Fragment {
 
+
+
+    Unbinder unbinder;
 
     public MineFragment() {
         // Required empty public constructor
@@ -24,7 +35,16 @@ public class MineFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fr_mine, container, false);
+        View view = inflater.inflate(R.layout.fragment_fr_mine, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+
     }
 
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
