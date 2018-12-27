4e869cd9dfec80c5906c64341436026f59c854cb
betterMan001 <851428195@qq.com>
2018-12-27 21:26:14 +0800

    Revert "登录界面： 1.修改登录样式 2.增加短信验证的界面交互 3.替换云信的logo为自己app的logo"
                               This reverts commit ad3242b

--- app/src/main/java/com/ly/a316/ly_meetingroommanagement/activites/LoginActivity.java
+++ app/src/main/java/com/ly/a316/ly_meetingroommanagement/activites/LoginActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ly.a316.ly_meetingroommanagement.R;
@@ -15,6 +14,7 @@ import com.netease.nim.uikit.api.NimUIKit;
 import com.netease.nimlib.sdk.NIMClient;
 import com.netease.nimlib.sdk.RequestCallback;
 import com.netease.nimlib.sdk.StatusBarNotificationConfig;
+import com.netease.nimlib.sdk.auth.AuthService;
 import com.netease.nimlib.sdk.auth.LoginInfo;
 
 import butterknife.BindView;
@@ -30,78 +30,44 @@ public class LoginActivity extends BaseActivity {
     @BindView(R.id.login_s_pwd_et)
     EditText loginSPwdEt;
     private static final String TAG = "LoginActivity";
-    @BindView(R.id.verification_et)
-    EditText verificationEt;
-    @BindView(R.id.get_identifying_code)
-    TextView getIdentifyingCode;
-    @BindView(R.id.verification_ll)
-    LinearLayout verificationLl;
-    //切换登录模式的标记
-    boolean flag = false;
-    @BindView(R.id.password_tv)
-    TextView passwordTv;
-    @BindView(R.id.act_message_verification)
-    TextView actMessageVerification;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
     }
-
-    //监听事件
+//监听事件
     @OnClick({R.id.act_login_sign_up, R.id.login_button, R.id.act_message_verification, R.id.act_forget_password})
     public void onViewClicked(View view) {
-        Intent intent = new Intent();
+        Intent intent=new Intent();
         switch (view.getId()) {
             //跳转到注册界面
             case R.id.act_login_sign_up:
-                intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                intent=new Intent(LoginActivity.this,SignUpActivity.class);
                 startActivity(intent);
                 break;
-            //登录
+                //登录
             case R.id.login_button:
                 login();
                 break;
-            //短信验证登录
+                //短信验证登录
             case R.id.act_message_verification:
-                changeLoginMod();
                 break;
-            //找回密码
+                //找回密码
             case R.id.act_forget_password:
-                intent = new Intent(LoginActivity.this, ForgetPWDOneActivity.class);
+                intent=new Intent(LoginActivity.this,ForgetPWDOneActivity.class);
                 startActivity(intent);
                 break;
         }
 
     }
-
-    private void changeLoginMod() {
-        //正常账号登录转短信验证
-        if (flag == false) {
-            flag = true;
-            this.loginSPwdEt.setVisibility(View.GONE);
-            this.passwordTv.setText("验证码");
-            this.actMessageVerification.setText("密码登录");
-            this.verificationLl.setVisibility(View.VISIBLE);
-        } else {
-            flag = false;
-            this.verificationLl.setVisibility(View.GONE);
-            this.passwordTv.setText("密码");
-            this.actMessageVerification.setText("验证码登录");
-            this.loginSPwdEt.setVisibility(View.VISIBLE);
-        }
-
-    }
-
-    void login() {
+    void login(){
         /*
         测试：不登陆账号
         */
-        final String account = "badMan";
-        String token = "123456";
-        LoginInfo info = new LoginInfo(account, token); // config...
+        final String account="badMan";
+        String token="123456";
+        LoginInfo info = new LoginInfo(account,token); // config...
         RequestCallback<LoginInfo> callback =
                 new RequestCallback<LoginInfo>() {
                     // 可以在此保存LoginInfo到本地，下次启动APP做自动登录用
@@ -127,13 +93,12 @@ public class LoginActivity extends BaseActivity {
                         Log.d(TAG, "onException: 登录异常！");
                     }
                 };
-        NimUIKit.login(info, callback);
+        NimUIKit.login(info,callback);
 
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        Intent intent=new Intent(LoginActivity.this,MainActivity.class);
         startActivity(intent);
         finish();
     }
-
     private void initNotificationConfig() {
         // 初始化消息提醒（先默认开启）
         NIMClient.toggleNotification(UserPreferences.getNotificationToggle());
@@ -145,9 +110,5 @@ public class LoginActivity extends BaseActivity {
         }
         // 更新配置
         NIMClient.updateStatusBarNotificationConfig(statusBarNotificationConfig);
-    }
-
-    @OnClick(R.id.act_message_verification)
-    public void onViewClicked() {
     }
 }
