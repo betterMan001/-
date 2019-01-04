d7c3a6772e74cd0c31e2f5761b53efd6e867a184
betterMan001 <851428195@qq.com>
2019-01-04 19:30:41 +0800

    1.会议室详情页面构思

--- app/src/main/java/com/ly/a316/ly_meetingroommanagement/activites/OrderDetailMeetingActivity.java
+++ app/src/main/java/com/ly/a316/ly_meetingroommanagement/activites/OrderDetailMeetingActivity.java
@@ -1,15 +1,33 @@
 package com.ly.a316.ly_meetingroommanagement.activites;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
+import com.gyf.barlibrary.ImmersionBar;
 import com.ly.a316.ly_meetingroommanagement.R;
 
-public class OrderDetailMeetingActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class OrderDetailMeetingActivity extends BaseActivity {
+
+    @BindView(R.id.customize_bar_rl)
+    RelativeLayout customizeBarRl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ImmersionBar.with(this).reset().fitsSystemWindows(true).statusBarColor(R.color.white).init();
         setContentView(R.layout.activity_order_detail_meeting);
+        ButterKnife.bind(this);
+        initView();
+    }
+private void initView(){
+        customizeBarRl.setBackgroundColor(getResources().getColor(R.color.white));
+}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ImmersionBar.with(this).destroy();
     }
 }
