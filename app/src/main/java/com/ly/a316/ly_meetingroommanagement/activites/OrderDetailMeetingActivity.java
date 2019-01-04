6469ad92083f756741075f7085dd3467b1ba9ea1
betterMan001 <851428195@qq.com>
2019-01-04 17:43:34 +0800

    1.暂时

---
+++ app/src/main/java/com/ly/a316/ly_meetingroommanagement/activites/OrderDetailMeetingActivity.java
@@ -0,0 +1,15 @@
+package com.ly.a316.ly_meetingroommanagement.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ly.a316.ly_meetingroommanagement.R;
+
+public class OrderDetailMeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail_meeting);
+    }
+}
