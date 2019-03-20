ad73e9883067e4c9cf4d63fe8cc997d201f99c9e
1169927533 <1169927533>
2019-03-20 09:43:59 +0800

    修改了样式

---
+++ app/src/main/java/com/ly/a316/ly_meetingroommanagement/Face.java
@@ -0,0 +1,17 @@
+package com.ly.a316.ly_meetingroommanagement;
+
+import com.arcsoft.facetracking.AFT_FSDKFace;
+
+import java.util.List;
+
+/**
+ * 作者：余智强
+ * 2019/3/19
+ */
+public class Face {
+    List<AFT_FSDKFace> list;
+    public static String  face;
+    public void getAllFace(){
+
+    }
+}
